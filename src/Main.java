93aea275ae5df5d9c6543a125f42ddaa059593cf
Tommy Abelsen <tomyabe93@gmail.com>
2017-03-29 01:06:52 +0200

    Added suggested solutions to tasks 1-8 with some alternatives.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Tommy on 23.03.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Løsningsforslag lF = new Løsningsforslag();
+
+
+        lF.primesSieve(1000);
+
+        //System.out.println((int)Math.ceil(Math.sqrt(6)));
+    }
+}
